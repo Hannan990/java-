5a18ca2581923d0715e673a091a47d450c34789f
Hannan990 <hannananxari990@gmail.com>
2024-10-29 19:03:06 +0000

    variables

---
+++ 01-Basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1
+let accountName = "Abdul"
+var accountType = "Asaan Account"
+accountCity = "Gujranwala"
+
+console.table([accountId, accountName, accountType, accountCity]);
+
+accountName = "Hannan"
+
+console.log(accountName);
