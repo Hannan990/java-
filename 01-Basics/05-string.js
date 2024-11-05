f427ba0235e08ffe49df00173f73490d9355c130
Hannan990 <hannananxari990@gmail.com>
2024-11-05 15:34:04 +0000

    String

---
+++ 01-Basics/05-string.js
@@ -0,0 +1,27 @@
+let name = "Abdul Hannan"
+let degree = "BSSE"
+// console.log(`Hello My Name is ${name} and My Degree Is ${degree}`);
+// console.log(name[0])
+// console.log(name.length);
+// console.log(name.charAt(2));
+// console.log(name.indexOf('n'));
+// console.log(name.toUpperCase());
+// console.log(name.italics())
+
+// let newName = name.substring(0, 5)
+// console.log(newName)
+
+// let newName = name.slice(0, 5)
+// console.log(newName);
+
+// let example = "bhai-yeh-example-hai-bs"
+// console.log(example.replace('-',' '))
+
+let example = "bhai-yeh-example-hai-bs"
+// console.log(example.replaceAll('-',' '))
+
+// console.log(example.includes("Bhai"))
+
+// console.log(example.split('-'))
+
+// ***********Also check notes that are on notepad**********
