a4bf4d068a44f8b74d49e8d4f2f154046a977720
Hannan990 <hannananxari990@gmail.com>
2024-11-05 19:17:25 +0000

    Number and Maths

---
+++ 01-Basics/06-Number_And_Maths.js
@@ -0,0 +1,32 @@
+let balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2)); // Used in e-commerce store to show precised amount (In decimals)
+
+// let newBalance = 123.8970
+// console.log(newBalance.toPrecision(4))
+
+let hundred = 10000
+// console.log(hundred.toLocaleString());
+
+// ++++++++++++++++++++++  MATHS ++++++++++++++++++++++++
+
+// console.log(Math.abs(-10))
+// console.log(Math.round(3.5))
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.1))
+// console.log(Math.sqrt(25))
+
+// console.log(Math.max(10,20,100))
+// console.log(Math.min(100,90,80));
+
+// console.log(Math.random())
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10));
+
+const maxNum = 30
+const minNum = 10
+
+console.log(Math.floor(Math.random() * (maxNum - minNum) +1 ) + minNum)
+
+console.log(Math.floor(Math.random() * (maxNum - minNum) +1 ) + minNum)
