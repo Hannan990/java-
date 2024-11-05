0bd7e867aa61000954bc6ca4e6357b1dfade7fd9
Hannan990 <hannananxari990@gmail.com>
2024-11-05 14:05:52 +0000

    Heap and Stack

---
+++ 01-Basics/Heap_and_Stack.js
@@ -0,0 +1,20 @@
+// let name1 = "Hannan"
+// let name2 = name1 
+// console.log(name2)
+// name2 = "Ali"
+// console.log(name2);
+// console.log(name1);
+
+let user1 = {
+    name : "Hannan",
+    id : 352
+}
+
+let user2 = user1
+console.log(user2);
+ user2.name = "Ali"
+
+console.log(user1);
+console.log(user2);
+
+
---
+++ 01-Basics/comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 <= 3)
+// console.log("2" == 2)
+// console.log("2" === 2)
+// console.log("2" <= 2)
+
+// let num1 = 100.10
+// console.log(typeof num1)
+
+const score = Symbol('123')
+const anotherScore = Symbol('123')
+
+console.log(score)
+console.log(anotherScore)
+console.log(typeof score)
+console.log(typeof anotherScore)
