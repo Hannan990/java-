70c756a5f2768f46f942b1dddebd9a26fd77c707
Hannan990 <hannananxari990@gmail.com>
2024-11-05 13:00:52 +0000

    Conversion Operations

---
+++ 01-Basics/conversionOperations.js
@@ -0,0 +1,22 @@
+// let score = "33abc"
+// console.log(score)
+
+// let valueInNumber = String(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let str1 = "Abdul"
+let str2 = " Hannan"
+let str3 = str1 + str2
+console.log(str3)
+ 
+console.log(+"")
+
+let isLoggedIn = 1
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)
+console.log(typeof boolIsLoggedIn)
+console.log(1 + 1 + "1")
+console.log("1" + 1 + 1)
+console.log(3**2)
