cd27ea3c6bcbc44cac34005092c3b47dd1e260ee
Hannan990 <hannananxari990@gmail.com>
2024-11-06 15:55:34 +0000

    Array1

---
+++ 02-Basics/01-arrays.js
@@ -0,0 +1,30 @@
+const array1 = [1,2,3,4]
+const array2 = ["Abdul", "Hannan", "Ansari"]
+const array3 = new Array(1,2,3,4)
+const array4 = new Array("Abdul" , "Hannan")
+
+// console.log("Array1 : ",array1)
+// console.log("Array2 : ",array2)
+// console.log("Array3 : ",array3)
+// console.log("Array4 : ",array4)
+
+array1.push(5)
+array1.push(6)
+array1.pop()
+
+// console.log("Array1 : ",array1)
+
+array2.unshift(0)
+array2.unshift(10)
+// console.log("Array2 : ",array2)
+array2.shift()
+// console.log("Array2 : ",array2)
+
+// const array5 = array1.slice(0,3)
+// console.log("Array1 : ",array1)
+// console.log("Array5 : ",array5)
+
+// const array6 = array2.splice(0,2)
+// console.log("Array2 : ",array2)
+// console.log("Array6 : ",array6)
+
