96c0ab31a4dd0c3277c010edb0fd486286c3f865
Hannan990 <hannananxari990@gmail.com>
2024-11-06 16:10:39 +0000

    Array2

---
+++ 02-Basics/02-arrays.js
@@ -0,0 +1,37 @@
+const array1 = ["Honda", "Toyota", "Supra"]
+const array2 = ["Lambo", "Ferarri", "Hyundai"]
+
+array1.push(array2)
+
+// console.log(array1)
+// console.log(array1[3][2])
+
+// const array3 = [...array1, ...array2]
+// console.log(array3)
+// console.log(array1)
+
+// const array3 = array1.flat(Infinity)
+// console.log(array3)
+// console.log(array1)
+
+// const name = "Hannan"
+// const name2 = "Ansari"
+// console.log(Array.isArray(name));
+// console.log(Array.isArray(array1));
+
+// const array3 = Array.from(name)
+// console.log(array3)
+
+// let number1 = 100
+// let number2 = 200
+// let number3 = 300
+
+// const array3 = Array.of(number1,number2,number3)
+// console.log(array3)
+
+const name1 = "Hannan"
+const name2 = "Ansari"
+const name3 = "Abdul"
+
+const array3 = Array.of(name1, name2, name3)
+console.log(array3)
