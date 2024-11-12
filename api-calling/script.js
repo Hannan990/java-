5b359d3b2129501edde0ae9400c222b9dd99c229
Hannan990 <hannananxari990@gmail.com>
2024-11-12 15:49:31 +0000

    api

---
+++ api-calling/script.js
@@ -0,0 +1,7 @@
+
+const button = document.getElementById(Search_Button);
+const input = document.getElementById(City_Input);
+
+button.addEventListener("click", () => {
+    console.log(input.value)
+});
