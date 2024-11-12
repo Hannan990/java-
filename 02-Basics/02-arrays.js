1d68764eadb63694b29b6f2ad963ea0c2818eefa
Hannan990 <hannananxari990@gmail.com>
2024-11-12 07:02:59 +0000

    Changes

--- 02-Basics/02-arrays.js
+++ 02-Basics/02-arrays.js
@@ -1,7 +1,7 @@
-const array1 = ["Honda", "Toyota", "Supra"]
-const array2 = ["Lambo", "Ferarri", "Hyundai"]
+// const array1 = ["Honda", "Toyota", "Supra"]
+// const array2 = ["Lambo", "Ferarri", "Hyundai"]
 
-array1.push(array2)
+// array1.push(array2)
 
 // console.log(array1)
 // console.log(array1[3][2])
@@ -29,9 +29,49 @@ array1.push(array2)
 // const array3 = Array.of(number1,number2,number3)
 // console.log(array3)
 
-const name1 = "Hannan"
-const name2 = "Ansari"
-const name3 = "Abdul"
+// const name1 = "Hannan"
+// const name2 = "Ansari"
+// const name3 = "Abdul"
 
-const array3 = Array.of(name1, name2, name3)
-console.log(array3)
+// const array3 = Array.of(name1, name2, name3)
+// console.log(array3)
+
+// const array1 = ["Honda", "Toyota", "Supra"]
+// const array2 = ["Lambo", "Ferarri", "Hyundai"]
+
+// const array3 = array1 + array2 
+// console.log(array3)
+// console.log(typeof array3)
+ 
+// const array3 = array1.map()
+
+
+let array1 = [1, 2, 3, 4, 5]
+let array2 = [6, 7, 8, 9, 10]
+
+// array1 = array2.copyWithin()
+// console.log(array1)
+
+// array1.fill(0, 2, 4)
+// console.log(array1)
+
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) 
+{
+    for (let i = 2; num > i; i++) 
+    {
+        if (num % i === 0) 
+        {
+        return false;
+        }
+    }
+    return num > 1;
+}
+
+// console.log(array.filter(isPrime))
+
+// array1.forEach( (element) => console.log(element))
+
+const map1 = array1.map((x) => x*2)
+console.log(map1)
